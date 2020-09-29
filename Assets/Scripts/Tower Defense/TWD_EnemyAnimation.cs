a7f8223e03f1e24f73c41edd759cab34cb3b2d6d
cniki006 <cniki006@edu.xamk.fi>
2020-09-29 15:37:55 +0300

    Paransin mapin, ratkaisin vihollisongelman

---
+++ Assets/Scripts/Tower Defense/TWD_EnemyAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TWD_EnemyAnimation : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = transform.localRotation * Quaternion.Inverse(transform.rotation);
+
+    }
+    private void LateUpdate()
+    {
+        
+    }
+}
